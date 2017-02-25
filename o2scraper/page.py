a31e6807b26614924dce31260a4ee7b17272bb28
James Holyhead <james@blupoint.org>
2017-02-25 14:22:04 +0000

    Added method to get the rate from tariff table, some minor bug fixes

--- o2scraper/page.py
+++ o2scraper/page.py
@@ -11,8 +11,11 @@ class BasePage(object):
     def title(self):
         return self.driver.title
 
-    def find_element(*loc):
+    def find_element(self, *loc):
         return self.driver.find_element(*loc)
+    
+    def find_elements(self, *loc):
+        return self.driver.find_elements(*loc)
     
     def open_page(self, url):
         self.driver.get(url)
@@ -26,12 +29,14 @@ class InternationalTariffsPage(BasePage):
     pay_and_go_button = (By.ID, "payandgo")
     tariffs_table = (By.ID, "standardRatesTable")
 
+    def __init__(self, driver):
+        BasePage.__init__(self, driver)
+
     def go_to(self):
         self.open_page(self.url)
 
     def search_for_country(self, country):
-        elem = self.find_element(*self.country_text_input).send_keys(country)
-        elem.send_keys(Keys.ENTER)
+        elem = self.find_element(*self.country_text_input).send_keys(country + Keys.ENTER)
 
     def select_tariff_type(self, tariff_type=Tariff.PAY_MONTHLY):
         if tariff_type is Tariff.PAY_MONTHLY:
@@ -40,7 +45,15 @@ class InternationalTariffsPage(BasePage):
             loc = self.pay_and_go_button 
         self.find_element(*loc).click()
 
-    
+    def get_rate(self, call_type=CallType.LANDLINE):
+        rates_table = self.find_element(*self.tariffs_table)
+        rates_rows = rates_table.find_elements(*(By.TAG_NAME, "tr"))
+        rate = ""
+        for row in rates_rows:
+            cells = row.find_elements(*(By.TAG_NAME, "td"))
+            if cells[0].text == call_type.value:
+                rate = cells[1].text
+        return rate
 
     
 
