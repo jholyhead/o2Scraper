7ebc3d29127d5bbfc74e094214fd49ad2a51052c
James Holyhead <james@blupoint.org>
2017-02-27 23:59:29 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='o2scraper',
       version='1.0',
-      py_modules=['o2scraper.scraper'],
+      py_modules=['o2scraper.scraper', 'o2scraper.page', 'o2scraper.o2'],
       )
